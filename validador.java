51c84c49f2c92462d45f1afe42b134c511159090
Victor-EduardoMota <vito142738@gmail.com>
2026-05-02 22:45:10 -0300

    validador de CPF

---
+++ validador.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class validador{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int primeiro_dig = 0;
+        int segundo_dig = 0;
+        System.out.println("digite seu CPF (apenas com numeros, ex: 12345678901)");
+        String cpf = sc.nextLine();
+        int soma = 0;
+        int soma2 = 0;
+        for(int i = 0; i <= 8; i++ ){
+
+            int cpf_num = cpf.charAt(i) - '0';
+            cpf_num *= (10 - i);
+            soma += cpf_num; 
+
+        }
+
+        int resto = soma % 11;
+            if (resto < 2) {
+                 primeiro_dig = 0;
+            }
+            else{
+                 primeiro_dig = 11 - resto;
+            }
+            
+
+        int primeiro = cpf.charAt(9) - '0';
+
+        if (primeiro_dig == primeiro) {
+            for(int a = 0; a <= 9; a++ ){
+
+            int cpf_num2 = cpf.charAt(a) - '0';
+            cpf_num2 *= (11 - a);
+            soma2 += cpf_num2; 
+
+        }
+        int resto2 = soma2 % 11;
+
+        if (resto2 < 2){
+            segundo_dig = 0;
+        }
+        else{
+            segundo_dig = 11 - resto2;
+        }
+
+        int segundo = cpf.charAt(10) - '0';
+
+        if (segundo == segundo_dig){
+            System.out.println("cpf valido");
+
+        }
+        else{
+            System.out.println("cpf invalido");
+        }
+
+        
+        }
+        else{
+            System.out.println("cpf invalido");
+
+        }
+sc.close();
+    }
+}
